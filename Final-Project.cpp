4da41fee3e18434416c09b293bb5624dbc18cb98
blanca890 <202310839@fit.edu.ph>
2024-07-04 14:22:13 +0800

    -add class video

--- Final-Project.cpp
+++ Final-Project.cpp
@@ -1,10 +1,20 @@
 #include<iostream>
+#include<string>
 
 using namespace std;
+
+class Video{
+    public:
+    int Video_ID;
+    string Movie Title;
+    string Genre;
+    string Production;
+    int Number_of_Copies;
+
+
+}
 
 int main() {
 
     return 0;
-
-    a
 }
